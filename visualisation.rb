fa01182f8e23648a4d41c8f9270ccdf712c55b42
LeonSinclair <lsinclai@tcd.ie>
2017-12-19 23:37:56 +0000

    Testing data. Changed error page on search failure.

--- visualisation.rb
+++ visualisation.rb
@@ -87,7 +87,7 @@ module Example
       repo_commits = Octokit.list_commits(repo_url)
       @commits_data = {}
       repo_commits.each do |single_commit|
-        commit_date = single_commit.author.date
+        puts commit_date = single_commit.author.date
         if !@commits_data[commit_date]
           @commits_data[commit_date] = 1
         else
