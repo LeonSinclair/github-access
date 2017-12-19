fcd25dad38f5e166857e9b1c0ffa60ee9935cce8
LeonSinclair <lsinclai@tcd.ie>
2017-12-19 14:37:49 +0000

    Further testing of problem.

--- visualisation.rb
+++ visualisation.rb
@@ -39,7 +39,7 @@ module Example
 
         @languages = []
         @language_obj.each do |lang, count|
-          @languages.push [:language => lang, :count => count]
+          @languages.push [lang, count]
         end
         puts languages
 
