351586114d312f08e2f15aa602d058a536e9b9c1
LeonSinclair <lsinclai@tcd.ie>
2017-12-20 00:03:06 +0000

    Extensive irb testing later. Data should be formatted correctly and accurate.

--- visualisation.rb
+++ visualisation.rb
@@ -87,7 +87,7 @@ module Example
       repo_commits = Octokit.list_commits(repo_url)
       @commits_data = {}
       repo_commits.each do |single_commit|
-        commit_date = single_commit.commit.author.date
+        commit_date = single_commit.commit.author.date.strftime("%F")
         if !@commits_data[commit_date]
           @commits_data[commit_date] = 1
         else
